93fdc0e20064cd189569be807517ecda1585fed4
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-21 11:13:49 -0400

    Kyle Thousand

--- server.js
+++ server.js
@@ -1,64 +1,68 @@
-const express = require('express')
-const logger = require('morgan')
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
-app.use(logger('dev'))
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(logger("dev"));
 
 app.listen(PORT, () => {
-    console.log(`Express server listening on port ${PORT}`);
-})
-
-const users =
-    [
-        {
-            id: "32e3ee5c-98dc-62a6-b284-0da231e3abb6",
-            username: "joe.e",
-            email: "joey@gmail.com",
-            password_digest: "OiJDs2sEycZEdkjdkhjsCHhQdJ8T9Lt2ns",
-            admin: true
-        }, {
-            id: "44b5fe5c-98dc-62a6-b284-0da231e3abb6",
-            username: "so.fia",
-            email: "sophia@gmail.com",
-            password_digest: "8eJ3s2s4ycGfdkjdkhjsCKhQdJ8T9Lt8ed",
-            admin: false
-        }
-    ]
-
-app.get('/', (req, res) => {
-    res.send("This is root!");
+  console.log(`Express server listening on port ${PORT}`);
 });
 
-app.get('/users', (req, res) => {
-    res.json(users)
-})
+const users = [
+  {
+    id: "32e3ee5c-98dc-62a6-b284-0da231e3abb6",
+    username: "joe.e",
+    email: "joey@gmail.com",
+    password_digest: "OiJDs2sEycZEdkjdkhjsCHhQdJ8T9Lt2ns",
+    admin: true,
+  },
+  {
+    id: "44b5fe5c-98dc-62a6-b284-0da231e3abb6",
+    username: "so.fia",
+    email: "sophia@gmail.com",
+    password_digest: "8eJ3s2s4ycGfdkjdkhjsCKhQdJ8T9Lt8ed",
+    admin: false,
+  },
+];
 
-app.get('/user/:id', (req, res) => {
-    const id = req.params.id
-    const user = users.filter(user => user.id === id)[0]
-    res.json(user)
-})
+app.get("/", (req, res) => {
+  res.send("This is root!");
+});
 
-app.post('/users', (req, res) => {
-    const user = req.body
-    users.push(user)
-    res.json(users)
-})
+app.get("/users", (req, res) => {
+  res.json(users);
+});
 
-app.put('/users/:id', (req, res) => {
-    const id = req.params.id
-    const userIndex = users.findIndex(user => user.id === id)
-    const user = { ...users[userIndex], ...req.body }
-    users.splice(userIndex, 1, user)
-    res.json(user)
-})
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  const user = users.filter((user) => user.id === id)[0];
+  res.json(user);
+});
 
-app.delete('/users/:id', (req, res) => {
-    const id = req.params.id
-    const userIndex = users.findIndex(user => user.id === id)
-    users.splice(userIndex, 1, user)
-    res.json(users)
-})
+app.post("/users", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.json(users);
+});
+
+app.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const userIndex = users.findIndex((user) => user.id === id);
+  const user = { ...users[userIndex], ...req.body };
+  users.splice(userIndex, 1, user);
+  res.json(user);
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const userIndex = users.findIndex((user) => user.id === id);
+  users.splice(userIndex, 1);
+  res.json(users);
+});
